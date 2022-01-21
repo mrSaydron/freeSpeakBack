b3e58c972f08688d54deedbf18e19a5da258dadc
karmanov.aiu <aleksei.karmanov@domru.ru>
2022-01-22 00:04:22 +0500

    s3 key

--- vm_scripts/docker_compose_up.sh
+++ vm_scripts/docker_compose_up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo docker login --username oauth --password AQAAAAAPJZSDAATuwanqBSRBy0stjeFyyENCOAA cr.yandex
+sudo docker-compose pull
 sudo docker-compose up --force-recreate --build -d
 sudo docker image prune -f
